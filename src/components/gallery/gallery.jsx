b3c0a61e5c75709d2472e7294543ebb7fea558bf
aamidou <akimayena26@gmail.com>
2025-04-24 23:26:40 +0100

    finish up with home page (gallery)

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,7 +1,70 @@
+import GalleryItem from '../galleryItem/galleryItem'
 import './gallery.css'
 
+//TEMP ITEM ARRAY
+
+const items = [
+  {
+    id: 1,
+    media: '/pins/pin1.jpeg',
+    width: 1260,
+    height: 1000,
+  },
+  {
+    id: 2,
+    media: '/pins/pin2.jpeg',
+    width: 1260,
+    height: 1400,
+  },
+  {
+    id: 3,
+    media: '/pins/pin3.jpeg',
+    width: 1260,
+    height: 1400,
+  },
+  {
+    id: 4,
+    media: '/pins/pin4.jpeg',
+    width: 1260,
+    height: 1000,
+  },
+  {
+    id: 5,
+    media: '/pins/pin5.jpeg',
+    width: 1260,
+    height: 1000,
+  },
+  {
+    id: 6,
+    media: '/pins/pin6.jpeg',
+    width: 1260,
+    height: 1400,
+  },
+  {
+    id: 7,
+    media: '/pins/pin7.jpeg',
+    width: 1260,
+    height: 500,
+  },
+  {
+    id: 8,
+    media: '/pins/pin8.jpeg',
+    width: 1260,
+    height: 1400,
+  },
+]
+
 const Gallery = () => {
-  return <div className="gallery">Gallery</div>
+  return (
+    <div className="gallery">
+      {items.map((item) => (
+        <GalleryItem
+          key={item.id.toString()}
+          item={item}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default Gallery
