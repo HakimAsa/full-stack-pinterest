9ab4a234314e08f3456539b05c87777151196ee8
aamidou <akimayena26@gmail.com>
2025-04-24 15:05:37 +0100

    style leftBar component

--- src/components/leftbar/leftBar.jsx
+++ src/components/leftbar/leftBar.jsx
@@ -1,7 +1,67 @@
 import './leftBar.css'
 
 const leftBar = () => {
-  return <div>leftBar</div>
+  return (
+    <div className="leftBar">
+      <div className="menuIcons">
+        <a
+          href="/"
+          className="menuIcon"
+        >
+          <img
+            src="/general/logo.png"
+            alt="logo"
+            className="logo"
+          />
+        </a>
+        <a
+          href="/"
+          className="menuIcon"
+        >
+          <img
+            src="/general/home.svg"
+            alt="home"
+          />
+        </a>
+        <a
+          href="/"
+          className="menuIcon"
+        >
+          <img
+            src="/general/create.svg"
+            alt="create"
+          />
+        </a>
+        <a
+          href="/"
+          className="menuIcon"
+        >
+          <img
+            src="/general/updates.svg"
+            alt="updates"
+          />
+        </a>
+        <a
+          href="/"
+          className="menuIcon"
+        >
+          <img
+            src="/general/messages.svg"
+            alt="messages"
+          />
+        </a>
+      </div>
+      <a
+        href="/"
+        className="menuIcon"
+      >
+        <img
+          src="/general/settings.svg"
+          alt="settings"
+        />
+      </a>
+    </div>
+  )
 }
 
 export default leftBar
