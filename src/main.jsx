e8fb817cc5bde928be7dd5130b90fd6f5a796b7b
aamidou <akimayena26@gmail.com>
2025-04-26 17:34:14 +0100

    add empty user profile page

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import Home from './pages/home/home.jsx'
 import Create from './pages/create/create.jsx'
 import Post from './pages/post/post.jsx'
 import Search from './pages/search/Search.jsx'
+import Auth from './pages/auth/auth.jsx'
+import Profile from './pages/profile/profile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -25,8 +27,16 @@ createRoot(document.getElementById('root')).render(
           element={<Post />}
         />
         <Route
+          path="/auth"
+          element={<Auth />}
+        />
+        <Route
           path="/search"
           element={<Search />}
+        />
+        <Route
+          path="/:username"
+          element={<Profile />}
         />
       </Routes>
     </BrowserRouter>
