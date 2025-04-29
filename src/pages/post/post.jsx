f9b15ae88b83b7dc6b497daa5817f771277b4b6d
aamidou <akimayena26@gmail.com>
2025-04-29 14:02:35 +0100

    prepare Post page and its sections

--- src/pages/post/post.jsx
+++ src/pages/post/post.jsx
@@ -1,7 +1,34 @@
+import { Link } from 'react-router'
+import Image from '../../components/image/image'
+import PostInteractions from '../../components/postInteractions/postInteractions'
 import './post.css'
+import Comments from '../../components/comments/comments'
 
 const Post = () => {
-  return <div className="post">Post</div>
+  return (
+    <div className="post">
+      <div className="postContainer">
+        <div className="postImage">
+          <Image
+            path="/pins/pin1.jpeg"
+            alt="pin1.jpeg"
+            width={736}
+          />
+        </div>
+        <div className="postDetails">
+          <PostInteractions />
+          <Link
+            to="/jonh"
+            className="postUser"
+          >
+            <Image path="/general/noAvatar.png" />
+            <span>John Doe</span>
+          </Link>
+          <Comments />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Post
